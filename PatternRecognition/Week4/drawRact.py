39bda7f077c90061ca09b8cc7f8ff29919dc6e9e
Sim-JuHeun <juheun9912@gmail.com>
2025-04-22 18:32:46 +0900

    [4주차] 코드 수정 및 실습

--- PatternRecognition/Week4/drawRact.py
+++ PatternRecognition/Week4/drawRact.py
@@ -2,17 +2,17 @@ import numpy as np
 import cv2
 
 blue, green, red = (255, 0, 0), (0, 255, 0), (0, 0, 255)
-image = np.zeros((400, 600, 3), np.uint8)
-image[:] = (255, 255, 255)
+image = np.zeros((400, 600, 3), np.uint8) # BGR 3채널로 컬러 윈도우를 만들겠다는 것
+image[:] = (255, 255, 255) # BGR 채널 값이 255로 순수 흰색 배경 생성
 
 pt1, pt2 = (50, 50), (250, 150)
 pt3, pt4 = (400, 150), (500, 50)
-roi = (50, 200, 200, 100)
+roi = (50, 200, 200, 100)       #왼쪽 상단 좌표 (50, 100) / weight = 200 / height 100
 
 cv2.line(image, pt1, pt2, red)
 cv2.line(image, pt3, pt4, green, 3, cv2.LINE_AA)
 
-cv2.rectangle(image, pt1, pt2, blue, 3, cv2.LINE_4)
+cv2.rectangle(image, pt1, pt2, blue, 3, cv2.LINE_4)  # pt1 , pt2 좌표를 대각선으로 하는 직사각형 생성
 cv2.rectangle(image, roi, red, 3, cv2.LINE_8)
 cv2.rectangle(image, (400,200,100,100), green, cv2.FILLED)
 
--- PatternRecognition/Week4/resizeWindow.py
+++ PatternRecognition/Week4/resizeWindow.py
@@ -1,22 +1,22 @@
 import cv2
 import numpy as np
 
-image = np.zeros((200, 400), np.uint8)
+image = np.zeros((200, 200), np.uint8)
 
-image[:] = 200
+image[:] = 0
 image
 
 title1, title2 = "position1", "position2"
 cv2.namedWindow(title1, cv2.WINDOW_AUTOSIZE)
 cv2.namedWindow(title2, cv2.WINDOW_NORMAL)
-cv2.moveWindow(title1, 0, 150)
-cv2.moveWindow(title2, 400, 50)
+cv2.moveWindow(title1, 0, 0)
+cv2.moveWindow(title2, 500, 50)
 
 
 cv2.imshow(title1, image)
 cv2.imshow(title2, image)
 
-cv2.resizeWindow(title1, 400, 300)
+cv2.resizeWindow(title1, 300, 300)
 cv2.resizeWindow(title2, 400, 300)
 
 cv2.waitKey(0)
